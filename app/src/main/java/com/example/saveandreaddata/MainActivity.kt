214c47c6c98af07ab552a50513651cb145ed5d79
mohamed tamer0 <mt4452242@gmail.com>
2021-02-15 23:17:13 +0200

    update

--- app/src/main/java/com/example/saveandreaddata/MainActivity.kt
+++ app/src/main/java/com/example/saveandreaddata/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.saveandreaddata
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.FileReader
 import java.io.FileWriter
@@ -15,8 +19,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
+    var WRITE_EXTERNAL_STORAGE = 255
+    var READ_EXTERNAL_STORAGE = 256
 
-    fun btnSaveClick(view:View) {
+    fun saveFile(){
         try {
             var fr = FileWriter(Environment.getExternalStorageDirectory().path + "/my_file.txt")
             fr.write(txt.text.toString())
@@ -28,7 +34,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun btnReadClick(view: View) {
+    fun readFile(){
         try {
             var fr = FileReader(Environment.getExternalStorageDirectory().path + "/my_file.txt")
             var x:Int = 0
@@ -42,6 +48,73 @@ class MainActivity : AppCompatActivity() {
             txt.setText(str)
         }catch (ex:Exception) {
             Toast.makeText(this@MainActivity,ex.message,Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    fun btnSaveClick(view:View) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),WRITE_EXTERNAL_STORAGE)
+        } else {
+            saveFile()
+        }
+    }
+
+    fun btnReadClick(view: View) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),READ_EXTERNAL_STORAGE)
+
+        } else {
+            readFile()
+        }
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            WRITE_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    saveFile()
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+
+            READ_EXTERNAL_STORAGE -> {
+                // If request is cancelled, the result arrays are empty.
+                if ((grantResults.isNotEmpty() &&
+                                grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    readFile()
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                }
+                return
+            }
+
+            // Add other 'when' lines to check for other
+            // permissions this app might request.
+            else -> {
+                // Ignore all other requests.
+            }
         }
     }
 
